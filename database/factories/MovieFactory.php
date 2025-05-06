f753cd1e12a93ad924f159e6a4fc5a68c2c48656
Sabran Hakim <sabranhakim00@gmail.com>
2025-05-06 10:31:31 +0700

    membuat seeder dan factory

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
@@ -16,8 +18,18 @@ class MovieFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->sentence(rand(3,6));
+        $slug = Str::slug($title);
         return [
-            //
+            'title' => $title,
+            'slug' => $slug,
+            'synopsis' => fake()->paragraph(),
+            'category_id' =>Category::inRandomOrder()->first(),
+            'year' => fake()->year(),
+            'actors' => fake()->name() . ',' . fake()->name() . ',' . fake()->name(),
+            'cover_image' => 'https://picsum.photos/seed/' . Str::random(10) . '/480/640',
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
